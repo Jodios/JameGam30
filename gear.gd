516e10724201efa318e02312613ff33b7c2ad9a5
Jodios <joelandapophis@gmail.com>
2023-07-30 13:10:15 -0700

    added a way for the gear to take damage

--- gear.gd
+++ gear.gd
@@ -1,9 +1,18 @@
 extends StaticBody2D
 
 @export var turn_speed: float = 1
+@export var health: float = 100.0
 
 func _ready():
+	#hit_box_area.body_entered.connect(take_damage)
 	pass
 
 func _physics_process(_delta):
 	rotation_degrees += turn_speed
+	
+func take_damage(damage: float):
+	health -= damage
+	for body in get_parent().get_children():
+		if body.has_method("grind_his_gears"):
+			body.grind_his_gears()
+			return
--- weapons/wrench/wrench.gd
+++ weapons/wrench/wrench.gd
@@ -1,14 +1,16 @@
 extends Sprite2D
 
 @export var direction: Vector2
+@export var damage: float = 10.0
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 
 func _ready():
 	$Area2D.body_entered.connect(check_collision)
 
 func _process(_delta):
-	position += direction  * 2
+	global_position += direction  * 5
 	rotation_degrees += 20
 	
 	if position.x < -1000 || position.x > 1000 || position.y < -1000 || position.y > 1000:
@@ -20,8 +22,16 @@ func check_collision(body):
 	direction = Vector2.ZERO
 	animation_player.speed_scale = 2
 	animation_player.play("blow_up")
+	if body.has_method("clockman"):
+		pass
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
 	await animation_player.animation_finished
 	queue_free()
-	if body.has_method("clockman"):
-		#do something when clockman gets hit
-		pass
+
+# function will be generic to all 
+# "weapons" that just returns the damage
+# so in other objects that take damage 
+# can check for weapon function
+func weapon() -> float:
+	return damage
