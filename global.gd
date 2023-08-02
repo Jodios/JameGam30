977ed0278d3576ba809a894323ef9c50cc7e4e57
Jodios <joelandapophis@gmail.com>
2023-08-01 20:55:26 -0700

    added timer

--- global.gd
+++ global.gd
@@ -3,3 +3,9 @@ extends Node
 var sound_volume: float = 0
 var music_volume: float = 0
 var level: int = 1
+var times_up: bool = false
+
+func _process(_delta):
+	if times_up:
+		Transition.dissolve("res://menus/death_screen/death_screen.tscn")
+		times_up = false
